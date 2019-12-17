3c346dda5b304d46ec8474b2f6bede4c30badfec
Violet M <vi@violet.wtf>
2019-12-16 22:42:56 -0800

    Fix identiation for internal/punish_user

--- internal/punish_user.go
+++ internal/punish_user.go
@@ -1,63 +1,63 @@
 package internal
 
 import (
-	"strings"
+    "strings"
 
-	"github.com/bwmarrin/discordgo"
-	"github.com/violetwtf/gopher/commands"
+    "github.com/bwmarrin/discordgo"
+    "github.com/violetwtf/gopher/commands"
 )
 
 // GetPunishUserCommand gets the command for a specific punishment action
 func GetPunishUserCommand(punishment string, permission int) commands.Command {
-	return commands.Command{
-		Definition: 
-			"Gives a user a " + punishment + ", and sends them the reason.",
-		Requirements: commands.CommandRequirements{
-			Args: []commands.Arg{
-				commands.Arg{Name: "user",   Type: "mention"},
-				commands.Arg{Name: "reason", Type: "string" }},
-			LongText:        true,
-			BotPermissions:  permission,
-			UserPermissions: permission,
-		},
-		Handler: func(
-			s *discordgo.Session, e *discordgo.MessageCreate, args []string,
-		) {
-			mentions := e.Mentions
+    return commands.Command{
+        Definition: 
+            "Gives a user a " + punishment + ", and sends them the reason.",
+        Requirements: commands.CommandRequirements{
+            Args: []commands.Arg{
+                commands.Arg{Name: "user",   Type: "mention"},
+                commands.Arg{Name: "reason", Type: "string" }},
+            LongText:        true,
+            BotPermissions:  permission,
+            UserPermissions: permission,
+        },
+        Handler: func(
+            s *discordgo.Session, e *discordgo.MessageCreate, args []string,
+        ) {
+            mentions := e.Mentions
 
-			target := mentions[0].ID
+            target := mentions[0].ID
 
-			dm, err := s.UserChannelCreate(target)
+            dm, err := s.UserChannelCreate(target)
 
-			if err != nil {
-				return
-			}
+            if err != nil {
+                return
+            }
 
-			guildID := e.GuildID
-			reason := strings.Join(args[1:], " ")
+            guildID := e.GuildID
+            reason := strings.Join(args[1:], " ")
 
-			guild, err := s.State.Guild(guildID)
+            guild, err := s.State.Guild(guildID)
 
-			if err != nil {
-				return
-			}
+            if err != nil {
+                return
+            }
 
-			// Send before the punishment, so that we can still MSG if they have
-			// mutuals DMs on
-			s.ChannelMessageSend(
-				dm.ID,
-				"You have recieved a **"+punishment+"** from **"+guild.Name+
-					"** for **"+reason+"**.")
+            // Send before the punishment, so that we can still MSG if they have
+            // mutuals DMs on
+            s.ChannelMessageSend(
+                dm.ID,
+                "You have recieved a **"+punishment+"** from **"+guild.Name+
+                    "** for **"+reason+"**.")
 
-			switch punishment {
-				case "ban":
-					go s.GuildBanCreateWithReason(guildID, target, reason, 0)
-					return
-				case "kick":
-					go s.GuildMemberDeleteWithReason(guildID, target, reason)
-					return
-				default:
-			}
-		},
-	}
+            switch punishment {
+                case "ban":
+                    go s.GuildBanCreateWithReason(guildID, target, reason, 0)
+                    return
+                case "kick":
+                    go s.GuildMemberDeleteWithReason(guildID, target, reason)
+                    return
+                default:
+            }
+        },
+    }
 }
